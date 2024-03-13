3b9cd8a572a3785baeb9c13c065a37bfd66f34aa
dewnithathsara <139765543+dewnithathsara@users.noreply.github.com>
2024-03-13 20:45:20 +0530

    Update User.java

--- User.java
+++ User.java
@@ -1,43 +1,52 @@
 package org.example.books.entities;
 
 
+import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 //import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 @Entity
+@Table(name = "users")
 public class User {
  
-
 @Id
-  private String Id;
-  private String name;
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name="Id")
+  private Long Id;
+ @Column(name="username")
   private String username;
+  @Column(name="email")
   private String emailAddress;
+  @Column(name="password")
   private String password;
- 
+  @OneToMany(mappedBy = "user")
+    private List<Transaction> transactions;
 
   public User(){
 
   }
-  public User(String Id,String name,String username, String emailAddress,String password){
-    Id=this.Id;
-    name=this.name;
+  public User(String username, String emailAddress,String password){
+    
+    
     username=this.username;
     emailAddress=this.emailAddress;
     password=this.password;
   }
-  public String getId() {
+ 
+  
+  public Long getId() {
     return Id;
   }
-  public void setId(String id) {
+  public void setId(Long id) {
     Id = id;
   }
-  public String getName() {
-    return name;
-  }
-  public void setName(String name) {
-    this.name = name;
-  }
+  
   public String getUsername() {
     return username;
   }
@@ -58,8 +67,15 @@ public class User {
   }
   @Override
   public String toString() {
-    return "User [Id=" + Id + ", name=" + name + ", username=" + username + ", emailAddress=" + emailAddress
-        + ", password=" + password + "]";
+    return "User [Id=" + Id + ", username=" + username + ", emailAddress=" + emailAddress + ", password=" + password
+        + ", transactions=" + transactions + "]";
+  }
+  public List<Transaction> getTransactions() {
+    return transactions;
+  }
+  public void setTransactions(List<Transaction> transactions) {
+    this.transactions = transactions;
   }
   
 }
+
