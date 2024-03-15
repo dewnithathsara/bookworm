63636a19b7485570651881b5a355a82b2f112019
dewnithathsara <139765543+dewnithathsara@users.noreply.github.com>
2024-03-15 08:47:52 +0530

    update file

--- module-info.java
+++ module-info.java
@@ -1,11 +1,16 @@
 module org.example.books {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.jfoenix;
     requires org.hibernate.orm.core;
     requires jakarta.persistence;
+    requires org.controlsfx.controls;
+    //requires mysql.connector.j;
      
   
     opens org.example.books.entities to org.hibernate.orm.core;
+    opens org.example.books.controller to javafx.fxml;
     opens org.example.books to javafx.fxml;
     exports org.example.books;
+    exports org.example.books.controller;
 }
